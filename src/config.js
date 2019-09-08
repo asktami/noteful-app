a8239452bc6168cc5849cab1de8d67bd12da57e1
Tami Williams <tami@asktami.com>
2019-09-08 12:21:54 -0400

    hosting db on github vs locally

--- src/config.js
+++ src/config.js
@@ -1,7 +1,16 @@
 export default {
-	API_FOLDERS: `http://localhost:9090/folders`,
-	API_NOTES: `http://localhost:9090/notes`
+	API_FOLDERS: `https://my-json-server.typicode.com/asktami/react-router-context-noteful/folders`,
+	API_NOTES: `https://my-json-server.typicode.com/asktami/react-router-context-noteful/notes`
 };
+
+// hosted on github
+// https://my-json-server.typicode.com/asktami/react-router-context-noteful
+
+// hosted locally:
+// export default {
+// 	API_FOLDERS: `http://localhost:9090/folders`,
+// 	API_NOTES: `http://localhost:9090/notes`
+// };
 
 // see documentation at
 // http://localhost:9090
