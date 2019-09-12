9ae1cd05a45f1816627174707056c5b31a1fefce
Tami Williams <tami@asktami.com>
2019-09-12 13:50:12 -0400

    switch db host to github

--- src/config.js
+++ src/config.js
@@ -1,16 +1,16 @@
 // hosted on github
 // https://my-json-server.typicode.com/asktami/react-router-context-noteful
 
-// export default {
-// 	API_FOLDERS: `https://my-json-server.typicode.com/asktami/react-router-context-noteful/folders`,
-// 	API_NOTES: `https://my-json-server.typicode.com/asktami/react-router-context-noteful/notes`
-// };
+export default {
+	API_FOLDERS: `https://my-json-server.typicode.com/asktami/react-router-context-noteful/folders`,
+	API_NOTES: `https://my-json-server.typicode.com/asktami/react-router-context-noteful/notes`
+};
 
 // hosted locally:
-export default {
-	API_FOLDERS: `http://localhost:9090/folders`,
-	API_NOTES: `http://localhost:9090/notes`
-};
+// export default {
+// 	API_FOLDERS: `http://localhost:9090/folders`,
+// 	API_NOTES: `http://localhost:9090/notes`
+// };
 
 // see documentation at
 // http://localhost:9090
