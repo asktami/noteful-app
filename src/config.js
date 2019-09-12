cd71edab5869a533b1ad231931a69635bce47e1b
Tami Williams <tami@asktami.com>
2019-09-11 22:56:42 -0400

    switched db

--- src/config.js
+++ src/config.js
@@ -2,15 +2,15 @@
 // https://my-json-server.typicode.com/asktami/react-router-context-noteful
 
 // export default {
-// 	API_FOLDERS: `https://my-json-server.typicode.com/asktami/react-router-context-noteful/folders`,
-// 	API_NOTES: `https://my-json-server.typicode.com/asktami/react-router-context-noteful/notes`
-// };
+	API_FOLDERS: `https://my-json-server.typicode.com/asktami/react-router-context-noteful/folders`,
+	API_NOTES: `https://my-json-server.typicode.com/asktami/react-router-context-noteful/notes`
+};
 
 // hosted locally:
-export default {
-	API_FOLDERS: `http://localhost:9090/folders`,
-	API_NOTES: `http://localhost:9090/notes`
-};
+// export default {
+// 	API_FOLDERS: `http://localhost:9090/folders`,
+// 	API_NOTES: `http://localhost:9090/notes`
+// };
 
 // see documentation at
 // http://localhost:9090
