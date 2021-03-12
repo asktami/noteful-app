3b6327f824ba0b98f9ad71b68a264bdb6c3cd2b7
Tami Williams <tami@asktami.com>
2021-03-11 21:50:49 -0500

    updated config, re-ran tests for new snapshots

--- src/config.js
+++ src/config.js
@@ -6,6 +6,9 @@ export default {
 	API_KEY: process.env.REACT_APP_API_KEY,
 };
 
+// hosted on github using json db
+// https://my-json-server.typicode.com/asktami/react-router-context-noteful
+
 // hosted locally using PostgreSQL db
 // export default {
 // 	DATASOURCE: `postgresql`,
@@ -20,9 +23,7 @@ export default {
 // 	FOLDERS_ENDPOINT: `http://localhost:9090/folders`,
 // 	NOTES_ENDPOINT: `http://localhost:9090/notes`
 // };
-
 // see documentation at
 // http://localhost:9090
-
 // see data at
 // http://localhost:9090/db
