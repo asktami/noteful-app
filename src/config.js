6d53dde4d78d4fa98a9eac042e4f6dc74a857d8b
Tami Williams <tami@asktami.com>
2021-03-17 18:41:28 -0400

    updated for CORS

--- src/config.js
+++ src/config.js
@@ -1,8 +1,21 @@
+// NOTE: You cannot override NODE_ENV manually
+// The built-in environment variable is called NODE_ENV. You can read it from process.env.NODE_ENV.  Its in the .env file.
+
+// TO SEE THE PRODUCTION version locally:
+// 1. run 'npm run build'
+// 2. then, 'serve -s build'
+
+// See https://create-react-app.dev/docs/adding-custom-environment-variables/
+
+/*
+When you run npm start, it is always equal to 'development', when you run npm test it is always equal to 'test', and when you run npm run build to make a production bundle, it is always equal to 'production'
+*/
+
 // hosted on Heroku using PostgreSQL db
 const prod = {
 	DATASOURCE: `postgresql`,
-	NOTES_ENDPOINT: `https://asktami-cors-anywhere.herokuapp.com/https://asktami-noteful-api.herokuapp.com/api/notes`,
-	FOLDERS_ENDPOINT: `https://asktami-cors-anywhere.herokuapp.com/https://asktami-noteful-api.herokuapp.com/api/folders`,
+	NOTES_ENDPOINT: `https://asktami-noteful-api.herokuapp.com/api/notes`,
+	FOLDERS_ENDPOINT: `https://asktami-noteful-api.herokuapp.com/api/folders`,
 	API_KEY: process.env.REACT_APP_API_KEY,
 };
 
